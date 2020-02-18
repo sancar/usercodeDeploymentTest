c5e5d0c251ee0c32a544f723c2e5f8dbe8d4dac3
sancar <sancar@hazelcast.com>
2020-02-18 14:30:11 +0300

    use case for two members with same lamda. They currently override each other

---
+++ app2/src/main/java/foo/MemberAppRunnable.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package foo;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IExecutorService;
+import com.hazelcast.spi.properties.ClusterProperty;
+
+import java.io.Serializable;
+import java.util.concurrent.ExecutionException;
+
+public class MemberAppRunnable {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Config config = new Config();
+        config.setProperty(ClusterProperty.PHONE_HOME_ENABLED.getName(), "false");
+
+        config.getUserCodeDeploymentConfig().setEnabled(true);
+
+        JoinConfig join = config.getNetworkConfig().getJoin();
+        join.getMulticastConfig().setEnabled(false);
+        join.getTcpIpConfig().setEnabled(true).addMember("127.0.0.1:5701").addMember("127.0.0.1:5702");
+        HazelcastInstance member = Hazelcast.newHazelcastInstance(config);
+
+        IExecutorService executorService = member.getExecutorService("test");
+
+        while (true) {
+            Thread.sleep(5000);
+            executorService.executeOnAllMembers((Serializable & Runnable) () -> {
+                System.out.println("App2Runnable");
+            });
+        }
+
+    }
+}
